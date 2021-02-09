58e69a928a250c94ba8a71ad31997290526cc15e
Alberto <aldo10012@gmail.com>
2021-02-08 21:27:29 -0800

    first commit

---
+++ spd-1.3-group-project/spd-1.3-group-project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spd-1.3-group-project
+//
+//  Created by Alberto Dominguez on 2/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spd-1.3-group-project/spd-1.3-group-project/spd_1_3_group_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  spd_1_3_group_projectApp.swift
+//  spd-1.3-group-project
+//
+//  Created by Alberto Dominguez on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct spd_1_3_group_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
