785d07579bddf7e51eec2e3b52d5660cb19ac50d
MakeRafa <rafavazquez229@gmail.com>
2021-02-24 13:01:31 -0800

    Name of category bug error still needs fixing

--- yelp-for-food/yelp-for-food/CategorySelectionView.swift
+++ yelp-for-food/yelp-for-food/CategorySelectionView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 
-
 struct CategorySelectionView: View {
     
     var body: some View {
@@ -33,8 +32,9 @@ struct CategorySelectionView_Previews: PreviewProvider {
 
 
 struct CategoryCell: View {
+    var category: Category
     static let column = 2
-    static let row = 9
+    static let row = 12
     let width = (UIScreen.main.bounds.width / 2)-20
     var body: some View {
         ZStack{
@@ -42,9 +42,42 @@ struct CategoryCell: View {
             Image("gray")
                 .resizable()
                 .frame(width: width, height: width)
+                .cornerRadius(10, antialiased: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
             
-            Text("Hello")
-                .offset(y: 30)
+            Text("\(category.name)")
+                .offset(y: 50)
         }
     }
 }
+
+struct Category: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+var eachCategory = [
+    Category(name: "American"),
+    Category(name: "British"),
+    Category(name: "Canadian"),
+    Category(name: "Chinese"),
+    Category(name: "Dutch"),
+    Category(name: "Egyptian"),
+    Category(name: "French"),
+    Category(name: "Greek"),
+    Category(name: "Indian"),
+    Category(name: "Irish"),
+    Category(name: "Italian"),
+    Category(name: "Jamaican"),
+    Category(name: "Japanese"),
+    Category(name: "Kenyan"),
+    Category(name: "Malaysian"),
+    Category(name: "Mexican"),
+    Category(name: "Moroccan"),
+    Category(name: "Polish"),
+    Category(name: "Russian"),
+    Category(name: "Spanish"),
+    Category(name: "Thai"),
+    Category(name: "Tunisian"),
+    Category(name: "Turkish"),
+    Category(name: "Vietnamese"),
+]
